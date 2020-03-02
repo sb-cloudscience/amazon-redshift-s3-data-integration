2271956e7dd54fe5626718d24e550c484cd6ce52
sb-cloudscience <58289066+sb-cloudscience@users.noreply.github.com>
2020-03-03 02:10:56 +0530

    Create xetra-table.sql

---
+++ xetra-table.sql
@@ -0,0 +1,16 @@
+create table xetra(
+ISIN	varchar,
+Mnemonic	varchar,
+SecurityDesc	varchar,
+SecurityType	varchar,
+Currency	varchar,
+SecurityID	integer,
+Date	date,
+Time	timestamp,
+StartPrice	float,
+MaxPrice	float,
+MinPrice	float,
+EndPrice	float,
+TradedVolume	float,
+NumberOfTrades	integer
+);
